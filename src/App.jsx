3d8f142c913d703ca9d40969b6fcd74d9b442120
Daniel Rangel <danranngel@gmail.com>
2022-09-12 14:22:56 -0400

    rendering label size dynamically

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Canvas } from "./canvas";
+
+function App() {
+  return (
+    <div className='App'>
+      <Canvas />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/canvas.jsx
@@ -0,0 +1,84 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+
+export const Canvas = () => {
+  const [labelsCount, setlabelsCount] = useState(4);
+  const [rollWidth] = useState(17.5);
+  const [rollHeight] = useState(20);
+  const [rollDimmensions, setrollDimmensions] = useState({
+    width: rollWidth / 2,
+    height: rollHeight / 2,
+  });
+
+  const changeWidht = (e) => {
+    const currentValue = e.target.value;
+    const percentage = (currentValue / 12) * rollWidth;
+    setrollDimmensions({ ...rollDimmensions, width: percentage });
+  };
+
+  const changeHeight = (e) => {
+    const currentValue = e.target.value;
+    const percentage = (currentValue / 12) * rollWidth;
+
+    const currentAreataken =
+      labelsCount * (parseFloat(rollDimmensions.width) * currentValue);
+    console.log(currentAreataken);
+    console.log("width", parseFloat(rollDimmensions.width));
+    console.log("height", parseFloat(rollDimmensions.height));
+
+    setrollDimmensions({ ...rollDimmensions, height: percentage });
+  };
+
+  const validateInput = (e) => {
+    if (!(e.code.includes("Digit") || e.code.includes("Backspace"))) {
+      console.log(!e.code.includes("Digit") || !e.code.includes("Backspace"));
+      e.preventDefault();
+    }
+  };
+
+  return (
+    <main>
+      <div className='inputs'>
+        <input
+          className='width'
+          placeholder='width'
+          onKeyDown={validateInput}
+          onChange={changeWidht}
+          inputMode='numeric'
+        />
+        <input
+          className='length'
+          placeholder='height'
+          onChange={changeHeight}
+          onKeyDown={validateInput}
+          inputMode='numeric'
+        />
+      </div>
+      <div className='roll'>
+        {/*left side part of the roll*/}
+        <div className='roll_thickness'>
+          <div className='thickness_radius'></div>
+        </div>
+
+        {/*the width of the roll: the cylinder tha runs vertically*/}
+        <div className='roll_width--wrapper'>
+          <div className='roll_width'></div>
+        </div>
+
+        {/*The lenght of the roll: the sheet that runs vertcally */}
+        <div className='roll_length'>
+          {[...Array(labelsCount)].map((_, i) => (
+            <div
+              className='length_label'
+              style={{
+                width: `${rollDimmensions.width}rem`,
+                height: `${rollDimmensions.height}rem`,
+              }}
+              key={i}
+            ></div>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+};
