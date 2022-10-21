f3b5900d4257fa3bba6b30ef2ed0aa9bbe5b685f
Daniel Rangel <danranngel@gmail.com>
2022-10-21 12:15:11 -0400

    first version

--- src/roll.jsx
+++ src/roll.jsx
@@ -1,14 +1,66 @@
+import React, { useEffect, useState, useRef } from "react";
 import "./roll.scss";
-import artV from "./assets/sttark_vertical.png";
-import artH from "./assets/sttark_horizontal.png";
+import logo from "./assets/sttark_label_logo.png";
 
-export const Roll = ({ artOrientation }) => {
+const PreviewRoll = ({ labelDirection = 3, art, length, width }) => {
+  // ref
+  const rollLength = useRef();
+
+  // state
+  const [labelLength, setlabelLength] = useState();
+  const [labelCount, setLabelCount] = useState();
+
+  art = art ? art : logo;
+
+  let labelFlow;
+
+  switch (labelDirection) {
+    case 1:
+      labelFlow = "labelFlow1";
+      break;
+    case 2:
+      labelFlow = "labelFlow2";
+      break;
+    case 3:
+      labelFlow = "labelFlow3";
+      break;
+    case 4:
+      labelFlow = "labelFlow4";
+      break;
+  }
+
+  const calculateWidth = () => {
+    if (width >= 3 && width <= 12) {
+      return width * ((11 / 12) * 10);
+    }
+    return 110;
+  };
+
+  const calculateLength = () => {
+    const availableLength = rollLength.current.getBoundingClientRect();
+    console.log("calculation fn");
+
+    if (length >= 3 && length <= 17) {
+      const labelCount = availableLength.width / (length * 10);
+      setLabelCount(Math.ceil(labelCount));
+      setlabelLength(length * 10);
+      console.log("first");
+    } else {
+      const labelCount = availableLength.width / 30;
+      setLabelCount(Math.ceil(labelCount));
+      setlabelLength(100);
+      console.log("first");
+    }
+  };
+
+  useEffect(() => {
+    calculateLength();
+    calculateWidth();
+  }, [length, width]);
+
   return (
-    <main className='component-wrapper'>
-      <div className='inputs'></div>
-
-      {/* roll starts */}
-      <div className='roll'>
+    <div className='roll-wrapper'>
+      <div className='roll' style={{ height: `${calculateWidth()}px` }}>
         {/*left side part of the roll*/}
         <div className='roll_thickness'>
           <div className='thickness_core'></div>
@@ -18,30 +70,72 @@ export const Roll = ({ artOrientation }) => {
         <div className='roll_width--wrapper'>
           <div className='roll_width'>
             <div className='width_top-border'></div>
+            <div className='width_top-border border_thin'></div>
             <div className='width_label-wrapper'>
-              <div
-                className='label-wrapper_label'
-                style={{
-                  backgroundImage: `url(${artV})`,
-                }}
-              ></div>
+              <div className='label-wrapper_label-wrapper'>
+                <div
+                  className={`label-wrapper_label ${labelFlow}`}
+                  style={{
+                    backgroundImage: `url(${art})`,
+                  }}
+                ></div>
+              </div>
+              <div className='label-wrapper_label-wrapper'>
+                <div
+                  className={`label-wrapper_label ${labelFlow}`}
+                  style={{
+                    backgroundImage: `url(${art})`,
+                  }}
+                ></div>
+              </div>
             </div>
           </div>
         </div>
 
         {/*The length of the roll: the sheet that runs horizontally */}
-        <div className='roll_length '>
-          {[...Array(3)].map((_, i) => (
+        <div className='roll_length' ref={rollLength}>
+          {[...Array(labelCount)].map((_, i) => (
             <div
-              className='length_label'
-              style={{
-                backgroundImage: `url(${artV})`,
-              }}
+              className='length_label-wrapper'
               key={i}
-            ></div>
+              style={{ width: `${labelLength}px` }}
+            >
+              <div
+                className={`length_label ${labelFlow}`}
+                style={{
+                  backgroundImage: `url(${art})`,
+                }}
+              ></div>
+            </div>
           ))}
         </div>
       </div>
-    </main>
+    </div>
+  );
+};
+
+export const Roll = () => {
+  const [length, setlength] = useState();
+  const [width, setwidth] = useState();
+
+  return (
+    <>
+      <div className='inputs'>
+        <input
+          type='text'
+          placeholder='lenght'
+          onChange={(e) => setlength(e.target.value)}
+        />
+        <span> </span>
+        <input
+          type='text'
+          placeholder='width'
+          onChange={(e) => setwidth(e.target.value)}
+        />
+      </div>
+      <div className='component-wrapper '>
+        <PreviewRoll length={length} width={width} />
+      </div>
+    </>
   );
 };
