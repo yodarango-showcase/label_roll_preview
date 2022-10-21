8f4969bfb7af6995756be0128144cdaab0ee9d16
Daniel Rangel <danranngel@gmail.com>
2022-10-21 17:20:31 -0400

    attempt to version 2

--- src/roll.jsx
+++ src/roll.jsx
@@ -2,140 +2,62 @@ import React, { useEffect, useState, useRef } from "react";
 import "./roll.scss";
 import logo from "./assets/sttark_label_logo.png";
 
-const PreviewRoll = ({ labelDirection = 3, art, length, width }) => {
-  // ref
-  const rollLength = useRef();
+// initial roll dimensions
+const rollDimensions = {
+    width: 100,
+    height: 200,
+    thickness: 30,
+  },
+  { width, height, thickness } = rollDimensions;
 
-  // state
-  const [labelLength, setlabelLength] = useState();
-  const [labelCount, setLabelCount] = useState();
+const styles = {
+    roll: {
+      width: `${width}px`,
+      height: `${height}px`,
 
-  art = art ? art : logo;
+      cylinder: {
+        borderBottomLeftRadius: `${width * 0.6}px ${height * 0.3}px `,
+        backgroundImage: `linear-gradient(
+      60deg,
+      #ffffff ${50 + width * 0.06}%,
+      #C6C3C1 50%
+    )`,
+      },
 
-  let labelFlow;
+      rollThickness: {
+        height: `${thickness + width * 0.05}px`,
+        top: `-${(thickness + width * 0.05) / 2}px`,
+        boxShadow: `0px ${height * 0.02}px #c6c3c1`,
+      },
 
-  switch (labelDirection) {
-    case 1:
-      labelFlow = "labelFlow1";
-      break;
-    case 2:
-      labelFlow = "labelFlow2";
-      break;
-    case 3:
-      labelFlow = "labelFlow3";
-      break;
-    case 4:
-      labelFlow = "labelFlow4";
-      break;
-  }
+      rollLength: {
+        top: `${(thickness + width * 0.05) / 2 + height * 0.02}px`,
+        height: `${height - (thickness + width * 0.05) / 2 - height * 0.02}px`,
+        width: `${width * 2}px`,
+        borderBottomLeftRadius: `${width * 0.6}px ${height * 0.3}px`,
+      },
+    },
+  },
+  {
+    roll,
+    roll: { rollThickness },
+    roll: { rollLength },
+    roll: { cylinder },
+  } = styles;
 
-  const calculateWidth = () => {
-    if (width >= 3 && width <= 12) {
-      return width * ((11 / 12) * 10);
-    }
-    return 110;
-  };
+console.log(roll);
 
-  const calculateLength = () => {
-    const availableLength = rollLength.current.getBoundingClientRect();
-    console.log("calculation fn");
-
-    if (length >= 3 && length <= 17) {
-      const labelCount = availableLength.width / (length * 10);
-      setLabelCount(Math.ceil(labelCount));
-      setlabelLength(length * 10);
-      console.log("first");
-    } else {
-      const labelCount = availableLength.width / 30;
-      setLabelCount(Math.ceil(labelCount));
-      setlabelLength(100);
-      console.log("first");
-    }
-  };
-
-  useEffect(() => {
-    calculateLength();
-    calculateWidth();
-  }, [length, width]);
-
+export const Roll = () => {
   return (
-    <div className='roll-wrapper'>
-      <div className='roll' style={{ height: `${calculateWidth()}px` }}>
-        {/*left side part of the roll*/}
-        <div className='roll_thickness'>
+    <div className='component-wrapper'>
+      <div className='roll' style={roll}>
+        <div className='roll_thickness' style={rollThickness}>
           <div className='thickness_core'></div>
         </div>
-
-        {/*the width of the roll: the cylinder tha runs horizontally*/}
-        <div className='roll_width--wrapper'>
-          <div className='roll_width'>
-            <div className='width_top-border'></div>
-            <div className='width_top-border border_thin'></div>
-            <div className='width_label-wrapper'>
-              <div className='label-wrapper_label-wrapper'>
-                <div
-                  className={`label-wrapper_label ${labelFlow}`}
-                  style={{
-                    backgroundImage: `url(${art})`,
-                  }}
-                ></div>
-              </div>
-              <div className='label-wrapper_label-wrapper'>
-                <div
-                  className={`label-wrapper_label ${labelFlow}`}
-                  style={{
-                    backgroundImage: `url(${art})`,
-                  }}
-                ></div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/*The length of the roll: the sheet that runs horizontally */}
-        <div className='roll_length' ref={rollLength}>
-          {[...Array(labelCount)].map((_, i) => (
-            <div
-              className='length_label-wrapper'
-              key={i}
-              style={{ width: `${labelLength}px` }}
-            >
-              <div
-                className={`length_label ${labelFlow}`}
-                style={{
-                  backgroundImage: `url(${art})`,
-                }}
-              ></div>
-            </div>
-          ))}
+        <div className='cylinder' style={cylinder}>
+          <div className='roll_length' style={rollLength}></div>
         </div>
       </div>
     </div>
-  );
-};
-
-export const Roll = () => {
-  const [length, setlength] = useState();
-  const [width, setwidth] = useState();
-
-  return (
-    <>
-      <div className='inputs'>
-        <input
-          type='text'
-          placeholder='lenght'
-          onChange={(e) => setlength(e.target.value)}
-        />
-        <span> </span>
-        <input
-          type='text'
-          placeholder='width'
-          onChange={(e) => setwidth(e.target.value)}
-        />
-      </div>
-      <div className='component-wrapper '>
-        <PreviewRoll length={length} width={width} />
-      </div>
-    </>
   );
 };
