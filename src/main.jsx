fb7535eed65705115e19c10489fedd5aed1db45d
Daniel Rangel <danranngel@gmail.com>
2022-10-25 08:57:03 -0400

    changing px to %

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Roll } from "./roll";
+import { RollPreview } from "./roll";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Roll />
+    <RollPreview />
   </React.StrictMode>
 );
--- src/roll.jsx
+++ src/roll.jsx
@@ -4,8 +4,8 @@ import logo from "./assets/sttark_label_logo.png";
 
 // initial roll dimensions
 const rollDimensions = {
-    width: 100,
-    height: 200,
+    width: 110,
+    height: 110,
     thickness: 30,
   },
   { width, height, thickness } = rollDimensions;
@@ -18,8 +18,8 @@ const styles = {
       cylinder: {
         borderBottomLeftRadius: `${width * 0.6}px ${height * 0.3}px `,
         backgroundImage: `linear-gradient(
-      60deg,
-      #ffffff ${50 + width * 0.06}%,
+      90deg,
+      #ffffff 50%,
       #C6C3C1 50%
     )`,
       },
@@ -36,6 +36,11 @@ const styles = {
         width: `${width * 2}px`,
         borderBottomLeftRadius: `${width * 0.6}px ${height * 0.3}px`,
       },
+
+      label: {
+        height: "80%",
+        //width: `${width / 3.2}%`, // decimal accounts for margin
+      },
     },
   },
   {
@@ -43,21 +48,59 @@ const styles = {
     roll: { rollThickness },
     roll: { rollLength },
     roll: { cylinder },
+    roll: { label },
   } = styles;
 
-console.log(roll);
+export const Roll = ({ labelWidth, labelLength }) => {
+  let width = labelWidth >= 4 ? `${labelWidth * 10}px` : "110px";
+  let length =
+    labelLength <= 2
+      ? `9.5%`
+      : labelLength > 2
+      ? `${100 / Math.ceil((rollDimensions.width * 2) / (labelLength * 10))}%`
+      : "31%";
 
-export const Roll = () => {
   return (
     <div className='component-wrapper'>
-      <div className='roll' style={roll}>
+      <div className='roll' style={{ ...roll, height: width }}>
         <div className='roll_thickness' style={rollThickness}>
           <div className='thickness_core'></div>
         </div>
         <div className='cylinder' style={cylinder}>
-          <div className='roll_length' style={rollLength}></div>
+          <div className='roll_length' style={rollLength}>
+            {[...Array(10)].map((_, i) => (
+              <div
+                key={i}
+                className='label'
+                style={{ ...label, width: length }}
+              ></div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
+  );
+};
+
+export const RollPreview = () => {
+  const [width, setWidth] = useState();
+  const [length, setLength] = useState();
+
+  return (
+    <>
+      <div className='inputs'>
+        <input
+          type='text'
+          placeholder='width'
+          onChange={(e) => setWidth(e.target.value)}
+        />
+        <input
+          type='text'
+          placeholder='height'
+          onChange={(e) => setLength(e.target.value)}
+        />
+      </div>
+      <Roll labelWidth={width} labelLength={length} />
+    </>
   );
 };
