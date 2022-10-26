25b004a6fde6fe15f4bbabf4c874138ca5d07047
Daniel Rangel <danranngel@gmail.com>
2022-10-26 11:25:46 -0400

    added support for shapes

--- src/roll.jsx
+++ src/roll.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState, useRef } from "react";
 import "./roll.scss";
 import logo from "./assets/sttark_label_logo.png";
+import { getDimensions } from "./helpers/get_dimensions";
+import { getShape } from "./helpers/get_shape";
 
 const rollDimensions = {
   width: 110,
@@ -14,80 +16,15 @@ export const Roll = ({
   orientation,
   corners,
 }) => {
-  const getLabelCount = () => {
-    // get possible label count
-    const area = rollDimensions.width * 1.5;
+  // get dimensions
+  const { labelWidth, labelLength, labelCount } = getDimensions(
+    width,
+    length,
+    rollDimensions.width
+  );
 
-    // dimensions
-    let labelWidth;
-    let labelLength;
-    let labelCount;
-
-    // width
-    if (!width) {
-      // if there is not width but there is a length make it a square by assigning it the value of the length
-      if (labelLength && labelLength <= 12) {
-        labelWidth = labelLength;
-      } else {
-        labelWidth = 120; // else let it be the default value
-      }
-    } else if (width <= 4) {
-      // since both labels smallest size is 4, they will always be a square regardless of the dimensions. Make it a rectangle if the length is greater than the width
-      if (length > width && length <= 4) {
-        labelWidth = 30;
-      } else {
-        labelWidth = 40;
-      }
-    } else if (width >= 12) {
-      labelWidth = 120;
-    } else {
-      labelWidth = width * 10;
-    }
-
-    // length
-    if (!length) {
-      // if there is not length but there is a width make it a square by assigning it the value of the width
-      if (labelWidth) {
-        labelLength = labelWidth;
-      } else {
-        labelLength = 170; // else let it be the default value
-      }
-    } else if (length <= 4) {
-      // since both labels smallest size is 4, they will always be a square regardless of the dimensions. Make it a rectangle if the length is greater than the width
-      if (length < width && width <= 4) {
-        labelLength = 30;
-      } else {
-        labelLength = 40;
-      }
-    } else if (length >= 17) {
-      labelLength = 170;
-    } else {
-      labelLength = length * 10;
-    }
-
-    // count
-    if (!length) {
-      if (labelLength <= 60) {
-        labelCount = 3;
-      } else {
-        labelCount = 2;
-      }
-    } else if (length > 13) {
-      labelCount = 1;
-    } else if (length < 4) {
-      labelCount = 3;
-    } else {
-      labelCount = Math.ceil(area / (length * 10));
-    }
-
-    return { count: labelCount, width: labelWidth, length: labelLength };
-  };
-
-  console.log(getLabelCount());
-
-  let labelCount = getLabelCount().count;
-  let labelWidth = getLabelCount().width;
-  let labelLength = getLabelCount().length;
+  // get shape
+  const labelShape = getShape(shape);
 
   // label orientation
   let labelOrientation;
@@ -109,25 +46,6 @@ export const Roll = ({
       labelOrientation = "labelFlow3";
   }
 
-  // label shape
-  let labelShape;
-  switch (shape) {
-    case 1:
-      labelShape = "square";
-      break;
-    case 2:
-      labelShape = "round";
-      break;
-    case 3:
-      labelShape = "custom";
-      break;
-    case 4:
-      labelShape = "previous";
-      break;
-    default:
-      labelShape = "square";
-  }
-
   return (
     <div className='component-wrapper'>
       <div
@@ -146,23 +64,18 @@ export const Roll = ({
               }}
             >
               {[...Array(labelCount)].map((_, i) => (
-                <>
-                  {shape === 1 && (
-                    <div
-                      key={i}
-                      className={`label ${labelShape}`}
-                      style={{
-                        width: `${labelLength - labelLength * 0.15}px`,
-                        height: `${labelWidth - labelWidth * 0.15}px`,
-                      }}
-                    >
-                      <div className={`label-design ${labelOrientation}`}></div>
-                    </div>
-                  )}
-                  {shape === 2 && (
-                    <div className={`${labelShape}`} key={i}></div>
-                  )}
-                </>
+                <div
+                  key={i}
+                  className={`label ${labelShape}`}
+                  style={{
+                    width: `${labelLength - labelLength * 0.15}px`,
+                    height: `${labelWidth - labelWidth * 0.15}px`,
+                  }}
+                >
+                  <div className={`label-design`}>
+                    <div className={`orientation ${labelOrientation}`}></div>
+                  </div>
+                </div>
               ))}
             </div>
           </div>
