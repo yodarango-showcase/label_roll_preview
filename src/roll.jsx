8bc55f6de3a20902533708bd1e109b5b2ea6f1c8
Daniel Rangel <danranngel@gmail.com>
2022-11-02 11:32:39 -0400

    getting somewhere with html el

--- src/roll.jsx
+++ src/roll.jsx
@@ -53,35 +53,21 @@ export const Roll = ({
                     }}
                   >
                     <div className={`design-curved`}>
+                      {/*<svg
+                        viewBox='0 0 800 800'
+                        version='1.1'
+                        style='fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;'
+                      >
+                        <path d='M33.21,470.785C33.21,470.785 39,505.07 65.196,521.392C91.936,538.054 124.845,548.652 176.048,554.759C216.765,559.616 300.061,554.453 300.061,554.453C300.061,554.453 308.055,596.842 292.88,602.409C267.836,611.598 194.387,622.146 149.794,609.583C105.201,597.02 44.753,550.164 25.323,527.031C13.146,512.535 33.21,470.785 33.21,470.785Z' />
+                  </svg>*/}
                       <div
                         className='design'
                         style={{
                           backgroundImage:
-                            "url(https://images.unsplash.com/photo-1429552077091-836152271555?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=985&q=80)",
+                            "url(https://images.unsplash.com/photo-1667330102328-c9a08a73373a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80)",
                         }}
                       ></div>
                     </div>
-                    <svg>
-                      <defs>
-                        <clipPath id='svgPath'>
-                          <path
-                            d='M1 105.613V2C1 2 10.6344 9.83743 23.7097 12.3613C45.7312 16.6122 65 15.8151 65 15.8151L63.6237 139C63.6237 139 35.4086 134.159 23.7097 127.487C8.56989 118.853 1 105.613 1 105.613Z'
-                            stroke='#FA665D'
-                          />
-                          {/*              <path
-                            d='M1 93.5126
-                            V2
-                            C 1 2 8.52688 8.92211 18.7419 11.1513
-                            C 35.9462 14.9056 51 14.2017 51 14.2017
-                            L 49.9247 123
-                            C 49.9247 123 27.8817 118.725 18.7419 112.832
-                            C 6.91398 105.206 1 93.5126 1 93.5126
-                            Z'
-                            stroke='#FA665D'
-                      />*/}
-                        </clipPath>
-                      </defs>
-                    </svg>
                   </div>
                 ) : (
                   <div
