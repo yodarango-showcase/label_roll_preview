c1833ef5df3d9e8eef3a75c1015d731608448017
Daniel Rangel <danranngel@gmail.com>
2023-02-22 22:15:26 -0500

    about wrapped up version 4 of the role

--- src/revisions/four/helpers/get_dimensions.js
+++ src/revisions/four/helpers/get_dimensions.js
@@ -1,110 +1,86 @@
 export const getDimensions = (width, length, dimensions) => {
-  const area = dimensions ? dimensions : 340;
+	const area = dimensions ? dimensions : 340;
 
-  // dimensions
-  let labelWidth;
-  let labelLength;
-  let labelCount;
+	// dimensions
+	let labelWidth;
+	let labelLength;
+	let labelCount;
 
-  // width
-  if (!width) {
-    // if there is not width but there is a length make it a square by assigning it the value of the length
-    if (labelLength && labelLength <= 12) {
-      labelWidth = labelLength;
-    } else {
-      labelWidth = 120; // else let it be the default value
-    }
-  } else if (width >= 12) {
-    labelWidth = 120;
-  } else {
-    labelWidth = width * 10;
-  }
+	// width
+	if (!width) {
+		//if there is not width but there is a length make it a square by assigning it the value of the length
+		if (labelLength && labelLength <= 12) {
+			labelWidth = labelLength;
+		} else {
+			labelWidth = 120; // else let it be the default value
+		}
+	} else if (width >= 12) {
+		labelWidth = 120;
+	} else if (width <= 4) {
+		labelWidth = 40;
+	} else {
+		labelWidth = width * 10;
+	}
 
-  // length
-  if (!length) {
-    // if there is not length but there is a width make it a square by assigning it the value of the width
-    if (labelWidth) {
-      labelLength = labelWidth;
-    } else {
-      labelLength = 170; // else let it be the default value
-    }
-  } else if (length >= 17) {
-    labelLength = 170;
-  } else {
-    labelLength = length * 10;
-  }
+	// length
+	if (!length) {
+		//if there is not length but there is a width make it a square by assigning it the value of the width
+		if (labelWidth) {
+			labelLength = labelWidth;
+		} else {
+			labelLength = 170; // else let it be the default value
+		}
+	} else if (length >= 17) {
+		labelLength = 170;
+	} else if (length <= 4) {
+		labelLength = 40;
+	} else {
+		labelLength = length * 10;
+	}
 
-  // get the area that should be curved
-  let consideredCurvedArea = labelLength <= 10 ? 40 : 80;
+	if (width - length === 0) {
+		labelLength = 120;
+		labelWidth = 120;
+	} else if (width - length > 0 && width - length <= 3) {
+		let percentage = (width - length) / 10;
+		percentage = 120 * percentage;
+		labelWidth = 120;
+		labelLength = 120 - percentage;
+	} else if (length - width > 0 && length - width <= 3) {
+		let percentage = (length - width) / 10;
+		percentage = 120 * percentage;
+		labelLength = 120;
+		labelWidth = 120 - percentage;
+	}
 
-  // get the area that should be straight
-  labelCount = Math.floor(area / (labelLength + 5)); // 5 accounts for the margin set
-  labelCount =
-    labelCount > 20
-      ? 20
-      : (labelCount <= 2) & (labelLength <= 140)
-      ? 3
-      : (labelCount <= 2) & (labelLength > 140)
-      ? 2
-      : labelCount;
+	// get the area that should be straight
+	labelCount = Math.floor(area / (labelLength + 5)); // 5 accounts for the margin set
+	//console.log(labelLength, area);
+	labelCount = labelCount > 20 ? 20 : (labelCount <= 2) & (labelLength <= 140) ? 3 : (labelCount <= 2) & (labelLength > 140) ? 2 : labelCount;
 
-  // construct the labels object
-  const labels = [...Array(labelCount)].map((_, index) => {
-    if (labelLength <= consideredCurvedArea || index === 0) {
-      consideredCurvedArea = consideredCurvedArea - labelLength;
+	return {
+		labels: [...Array(labelCount)],
+		labelWidth,
+		labelLength,
+	};
+};
 
-      return { id: index, isCurved: true };
-    } else {
-      // heal the gap between curved and straight area
-      if (labelLength <= 20) {
-        return {
-          id: index,
-          isCurved: false,
-          addStyles: { transform: `translateX(-13px)` },
-        };
-      }
-      if (labelLength <= 40) {
-        return {
-          id: index,
-          isCurved: false,
-          addStyles: { transform: `translateX(-5px)` },
-        };
-      }
-      // if (labelLength >= 100) {
-      //   return {
-      //     id: index,
-      //     isCurved: false,
-      //     addStyles: { transform: `translateX(-7px)` },
-      //   };
-      // }
-      if (labelLength >= 80) {
-        if (labelWidth >= 100) {
-          return {
-            id: index,
-            isCurved: false,
-            addStyles: { transform: `translateX(-4px)` },
-          };
-        } else {
-          return {
-            id: index,
-            isCurved: false,
-            addStyles: { transform: `translateX(-7px)` },
-          };
-        }
-      }
-      if (labelLength >= 40) {
-        return {
-          id: index,
-          isCurved: false,
-          addStyles: { transform: `translateX(-3px)` },
-        };
-      }
-    }
-  });
+export const calculateRadius = (squaredCorners, shape, labelWidth, labelLength) => {
+	if (squaredCorners) {
+		return "0px";
+	}
 
-  return {
-    labels,
-    labelWidth,
-    labelLength,
-  };
+	if (shape === 1) {
+		if (labelWidth >= 4 && labelLength >= 4) {
+			return `.5em`;
+		} else {
+			return `.3em`;
+		}
+	}
+
+	if (shape === 2) {
+		return "50%";
+	}
+
+	return "0px";
 };
--- src/revisions/four/roll_preview.jsx
+++ src/revisions/four/roll_preview.jsx
@@ -1,8 +1,7 @@
 import { getOrientation, getShape } from "./helpers/get_options";
-import { getDimensions } from "./helpers/get_dimensions";
+import { calculateRadius, getDimensions } from "./helpers/get_dimensions";
 import { useState, useRef, useEffect } from "react";
 import curvedLengthLarge from "./assets/curved_length_large.png";
-import curvedLengthSmall from "./assets/curved_length_small.png";
 import "./roll_preview.scss";
 
 const rollDimensions = {
@@ -10,30 +9,18 @@ const rollDimensions = {
 	length: 340,
 };
 
-export const PreviewRoll4 = ({ width = 12, length = 12, shape = 1, orientation = 3, squaredCorners = false }) => {
+export const PreviewRoll4 = ({ width = 12, length = 12, shape = 1, orientation = 1, squaredCorners = false }) => {
 	const [rollLength, setRollLength] = useState(null);
 	let rollLengthDOMEL = useRef(null);
 
-	// corner radius
-	let cornerRadius;
-
-	if (shape === 1) {
-		if (squaredCorners) {
-			cornerRadius = "0px";
-		} else {
-			cornerRadius = `${(width + length) * 0.35}px`;
-		}
-	} else if (shape === 2) {
-		cornerRadius = "50%";
-	} else {
-		if (squaredCorners) cornerRadius = "0px";
-	}
-
-	// decide which curved length to render
-	const curvedLength = width >= 5 ? curvedLengthLarge : curvedLengthSmall;
+	// const get the core's size
+	let coreSize = length <= 17 ? `${59 - length * 2}%` : "25%";
 
 	// get dimensions
 	const { labelWidth, labelLength, labels } = getDimensions(width, length, rollLength);
+
+	// corner radius
+	const cornerRadius = calculateRadius(squaredCorners, shape, labelWidth, labelLength);
 
 	// get shape
 	const labelShape = getShape(shape);
@@ -41,7 +28,7 @@ export const PreviewRoll4 = ({ width = 12, length = 12, shape = 1, orientation =
 	// label orientation
 	let labelOrientation = getOrientation(orientation);
 
-	const noLables = labels.length === 0 || !labels || shape > 3;
+	const labelsExists = labels && labels.length > 0;
 
 	useEffect(() => {
 		const rollL = rollLengthDOMEL.current.getBoundingClientRect();
@@ -53,37 +40,40 @@ export const PreviewRoll4 = ({ width = 12, length = 12, shape = 1, orientation =
 			{/* Entire roll element */}
 			<div className={`roll`} style={{ height: `${labelWidth}px`, width: rollDimensions.width }}>
 				{/* Thickness: The oval that goes on top of the roll showing the diameter (whole) and thickness of the roll */}
-				<div className={`roll_thickness ${labelWidth <= 40 && "min-width"}`}>
-					<div className='thickness_core'></div>
+				<div className={`roll_thickness`}>
+					<div className='thickness_core' style={{ width: coreSize, height: coreSize }}></div>
 				</div>
 
 				{/* Cylinder: The cylinder which the labels wrap around  */}
 				<div className='cylinder'>
 					{/* Length: The length of the roll that is curved as it unrolls */}
 					{/*<div className='roll-length-wrapper'>*/}
-					<img src={curvedLength} alt='curved roll length' className={`folded-roll-length s-${labelWidth}`} />
+					<img src={curvedLengthLarge} alt='curved roll length' className={`folded-roll-length `} />
 					{/* Length: The material that holds the labels. The label count is extrapolating the width props of this DOM Element*/}
 					<div
 						ref={rollLengthDOMEL}
 						id='roll_length'
-						className={`roll_length ${labelWidth <= 40 ? "min-width" : ""} ${noLables ? "min-height" : ""}`}
+						className={`roll_length ${labelWidth <= 40 ? "min-width" : ""}`}
 						style={{
 							minWidth: rollDimensions.length,
 						}}
 					>
 						{shape > 3 && <div className='loading'>Loading...</div>}
 						<div className={`label-wrapper`}>
-							{labels.map((label, i) => (
-								<div
-									key={i}
-									className={`label ${labelShape}`}
-									style={{
-										height: `${labelWidth}px`,
-										width: `${labelLength}px`,
-										borderRadius: `${cornerRadius}`,
-									}}
-								></div>
-							))}
+							{labelsExists &&
+								labels.map((label, i) => (
+									<div
+										key={i}
+										className={`label ${labelShape} `}
+										style={{
+											height: `${labelWidth}px`,
+											width: `${labelLength}px`,
+											borderRadius: `${cornerRadius}`,
+										}}
+									>
+										<div className={`orientation ${labelOrientation} orientation-${labelShape}`}></div>
+									</div>
+								))}
 						</div>
 					</div>
 					{/*</div>*/}
