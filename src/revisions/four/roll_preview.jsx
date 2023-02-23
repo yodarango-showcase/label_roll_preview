e70d829c7fb431a0c95b160053fc780a89a43d84
Daniel Rangel <danranngel@gmail.com>
2023-02-22 19:34:32 -0500

    added curved lenght

--- src/revisions/four/roll_preview.jsx
+++ src/revisions/four/roll_preview.jsx
@@ -1,10 +1,9 @@
 import { getOrientation, getShape } from "./helpers/get_options";
 import { getDimensions } from "./helpers/get_dimensions";
 import { useState, useRef, useEffect } from "react";
-import curvedLength from "./assets/curved_length.png";
+import curvedLengthLarge from "./assets/curved_length_large.png";
+import curvedLengthSmall from "./assets/curved_length_small.png";
 import "./roll_preview.scss";
-
-import { getSvgMask, transformMask, transformRoundShape, transformCustomShape } from "./helpers/get_coordinates";
 
 const rollDimensions = {
 	width: 160,
@@ -14,7 +13,24 @@ const rollDimensions = {
 export const PreviewRoll4 = ({ width = 12, length = 12, shape = 1, orientation = 3, squaredCorners = false }) => {
 	const [rollLength, setRollLength] = useState(null);
 	let rollLengthDOMEL = useRef(null);
-	const isSquaredCorners = squaredCorners ? "squared-corners" : "";
+
+	// corner radius
+	let cornerRadius;
+
+	if (shape === 1) {
+		if (squaredCorners) {
+			cornerRadius = "0px";
+		} else {
+			cornerRadius = `${(width + length) * 0.35}px`;
+		}
+	} else if (shape === 2) {
+		cornerRadius = "50%";
+	} else {
+		if (squaredCorners) cornerRadius = "0px";
+	}
+
+	// decide which curved length to render
+	const curvedLength = width >= 5 ? curvedLengthLarge : curvedLengthSmall;
 
 	// get dimensions
 	const { labelWidth, labelLength, labels } = getDimensions(width, length, rollLength);
@@ -24,21 +40,6 @@ export const PreviewRoll4 = ({ width = 12, length = 12, shape = 1, orientation =
 
 	// label orientation
 	let labelOrientation = getOrientation(orientation);
-
-	// // get the corresponding svg mask
-	// const SVGMask = getSvgMask(width, length, squaredCorners);
-
-	// // transform the square mask on size adjustment
-	// const SVGMaskTransform = transformMask(width, length, orientation);
-
-	// // transform the circular shape on size adjustment
-	// const roundTransform = transformRoundShape(width, length, orientation);
-
-	// // transform the circular shape on size adjustment
-	// const customTransform = transformCustomShape(width, length, orientation);
-
-	// // decide whether is round or custom shape
-	// const customOrRound = shape === 2 ? roundTransform : customTransform;
 
 	const noLables = labels.length === 0 || !labels || shape > 3;
 
@@ -59,32 +60,33 @@ export const PreviewRoll4 = ({ width = 12, length = 12, shape = 1, orientation =
 				{/* Cylinder: The cylinder which the labels wrap around  */}
 				<div className='cylinder'>
 					{/* Length: The length of the roll that is curved as it unrolls */}
-					<div className='roll-length-wrapper'>
-						<img src={curvedLength} alt='curved roll length' className='folded-roll-length' />
-						{/* Length: The material that holds the labels. The label count is extrapolating the width props of this DOM Element*/}
-						<div
-							ref={rollLengthDOMEL}
-							id='roll_length'
-							className={`roll_length ${labelWidth <= 40 && "min-width"} ${noLables ? "min-height" : ""}`}
-							style={{
-								minWidth: rollDimensions.length,
-							}}
-						>
-							{shape > 3 && <div className='loading'>Loading...</div>}
-							<div className={`label-wrapper`}>
-								{labels.map((label, i) => (
-									<div
-										key={i}
-										className={`label ${labelShape}  ${isSquaredCorners}`}
-										style={{
-											height: `${labelWidth}px`,
-											width: `${labelLength}px`,
-										}}
-									></div>
-								))}
-							</div>
+					{/*<div className='roll-length-wrapper'>*/}
+					<img src={curvedLength} alt='curved roll length' className={`folded-roll-length s-${labelWidth}`} />
+					{/* Length: The material that holds the labels. The label count is extrapolating the width props of this DOM Element*/}
+					<div
+						ref={rollLengthDOMEL}
+						id='roll_length'
+						className={`roll_length ${labelWidth <= 40 ? "min-width" : ""} ${noLables ? "min-height" : ""}`}
+						style={{
+							minWidth: rollDimensions.length,
+						}}
+					>
+						{shape > 3 && <div className='loading'>Loading...</div>}
+						<div className={`label-wrapper`}>
+							{labels.map((label, i) => (
+								<div
+									key={i}
+									className={`label ${labelShape}`}
+									style={{
+										height: `${labelWidth}px`,
+										width: `${labelLength}px`,
+										borderRadius: `${cornerRadius}`,
+									}}
+								></div>
+							))}
 						</div>
 					</div>
+					{/*</div>*/}
 				</div>
 			</div>
 		</div>
